5319528bc4f143ccdc5543c17a58ec986195ddc0
T4C30 <T4C30@outlook.es>
2026-02-04 10:58:44 +0100

    V0.0.2
    Creacion de una CLI , definicion la ejecucion del script y control de parametros

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from argparse import ArgumentParser
+from mod.mod import cli
+from mod.mod import ejecucion
 
 api_key = ""
 steam_id = ""
+webhook = ""
 
+parseador = ArgumentParser(
+    prog="SteamToGog",
+    description="Busqueda de juegos disponible de Steam a GOG"
+)
+
+parseador.add_argument("-t","--tui",action="store_false", help="Entra en modo TUI/CLI")
+parseador.add_argument("-g","--gui",action="store_true", help="Entra en modo GUI")
+parseador.add_argument("-a", help="API Key de desarrollador de Steam")
+parseador.add_argument("-s", help="Steam id del usuario")
+parseador.add_argument("-w", help="Webhook para enviar informacion")
+
+argumentos = parseador.parse_args()
 
 def main():
-    pass
-
+    if argumentos.tui:
+        api_key = argumentos.a
+        steam_id = argumentos.s
+        webhook = argumentos.w
+        ejecucion(api_key, steam_id, webhook)
+    elif argumentos.gui:
+        # Abrir una ventana con Tkinter
+        pass
+    else:
+        cli() 
 
 if "__main__" == __name__:
     main()
--- mod/mod.py
+++ mod/mod.py
@@ -2,7 +2,7 @@ from os import open
 from requests import get
 
 # Pedir en Steam los juegos totales del jugador
-def steam(api_key, steam_id):
+def steam(api_key:str, steam_id:str):
     respuesta=get(f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&format=json")
     json_respuesta=respuesta.json()
     juegos = json_respuesta["response"]["games"]
@@ -18,11 +18,11 @@ def steam(api_key, steam_id):
     return nombres
 
 # Comprobar que no esten en el archivo de enviados
-def abrir():
+def abrir(nombres:list):
     pass
 
 # Pedir en gog los juegos totales de la pagina
-def gog():
+def gog(juegos_steam:list):
     respuesta=get("https://catalog.gog.com/v1/catalog?limit=50&order=asc")
     json_respuesta=respuesta.json() 
     productos = []
@@ -34,13 +34,29 @@ def gog():
         juegos = json_respuesta["products"]
         for juego in juegos:
             productos.append(juego["title"])
+            
     return productos
 
 
 # Enviar a Discord
-def discord():
+def discord(webhook:str, juegos:list):
     pass
 
 # Guardar en el archivo
-def guardar():
+def guardar(juegos:list):
     pass
+
+def cli():
+    api_key = input("Api key del desarrollador: ")
+    steam_id = input("SteamID del jugador: ")
+    webhook = input("Dime la webhook de discord: ")
+    ejecucion(api_key,steam_id,webhook)
+    
+def ejecucion(api_key:str,steam_id:str,webhook:str):
+    nombres = steam(api_key,steam_id)
+    juegos_steam = abrir(nombres)
+    juegos = gog(juegos_steam)
+    discord(webhook,juegos)
+    guardar(juegos)
+    
+    
